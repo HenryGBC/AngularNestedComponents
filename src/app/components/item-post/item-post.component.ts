665a703587cb01ae9ef273a4c0ee6dd78b6ac211
unknown <henry@novap.legal>
2020-10-15 08:40:51 -0500

    Components finish

--- src/app/components/item-post/item-post.component.ts
+++ src/app/components/item-post/item-post.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-post',
   templateUrl: './item-post.component.html',
-  styleUrls: ['./item-post.component.css']
+  styleUrls: ['./item-post.component.css'],
 })
 export class ItemPostComponent implements OnInit {
+  @Input() value: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
